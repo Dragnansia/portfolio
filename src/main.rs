f43affe54588ff31e3e72124446f7361c7385db1
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-11-15 20:17:29 +0100

    add rocket and create home function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::FileServer;
+use rocket_dyn_templates::Template;
+use std::collections::HashMap;
+
+#[get("/")]
+fn home() -> Template {
+    let mut context: HashMap<String, String> = HashMap::new();
+    Template::render("home", context)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from("static"))
+        .mount("/", routes![home])
+        .attach(Template::custom(|_engines| {}))
 }
