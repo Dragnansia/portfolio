2a0d1d1fc114e7c74270087802ddf5b65f42ddf5
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-06 23:47:51 +0100

    update project asset path

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module = "/index.js")]
+#[wasm_bindgen(module = "/js/index.js")]
 extern "C" {
     #[wasm_bindgen(js_name = "moveTo")]
     pub fn move_to(name: &str);
