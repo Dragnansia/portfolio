6681580ec6dcbb5e0106a1b00f8f3854d00824af
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-07 23:05:56 +0100

    add arctix change

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+use std::{io, sync::Mutex};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Mutex::new(0))
+            .service(Files::new("/", "./dist/").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
