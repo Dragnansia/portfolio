9eafc97b091388736b0d8528f73c7fda97221cc2
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-08 16:43:54 +0100

    update bind host

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,21 @@
 use actix_files::Files;
 use actix_web::{App, HttpServer};
-use std::{io, sync::Mutex};
+use std::{env, io, sync::Mutex};
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    let host = env::var("HOST").unwrap_or("0.0.0.0".into());
+    let port = env::var("PORT")
+        .unwrap_or("8000".into())
+        .parse::<u16>()
+        .unwrap();
+
     HttpServer::new(move || {
         App::new()
             .app_data(Mutex::new(0))
             .service(Files::new("/", "./dist/").index_file("index.html"))
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind((host, port))?
     .run()
     .await
 }
