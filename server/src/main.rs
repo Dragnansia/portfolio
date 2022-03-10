25bee55a7b3ffef28e2c0694afde6841b2648c3f
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-10 18:06:04 +0100

    add mongodb connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,14 @@
 use actix_files::Files;
 use actix_web::{App, HttpServer};
+use mongodb::{error::Error, options::ClientOptions, Client};
 use std::{env, io, sync::Mutex};
+
+async fn db_connection() -> Result<Client, Error> {
+    let db_url = env::var("DB_URL").unwrap_or_default();
+    let options = ClientOptions::parse(db_url).await?;
+
+    Client::with_options(options)
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
@@ -10,8 +18,19 @@ async fn main() -> io::Result<()> {
         .parse::<u16>()
         .unwrap();
 
+    let db = match db_connection().await {
+        Ok(db) => db,
+        Err(error) => {
+            return Err(std::io::Error::new(
+                io::ErrorKind::Unsupported,
+                error.to_string(),
+            ));
+        }
+    };
+
     HttpServer::new(move || {
         App::new()
+            .app_data(db.clone())
             .app_data(Mutex::new(0))
             .service(Files::new("/", "./dist/").index_file("index.html"))
     })
