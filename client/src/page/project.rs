344b34bae90f8c2bc7ac8b6912eb7df2b0f7bd83
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-24 15:55:32 +0100

    update project view

--- client/src/page/project.rs
+++ client/src/page/project.rs
@@ -7,10 +7,10 @@ pub fn view(project: Option<Project>) -> Html {
         <div class="modal modal-full ie-scroll-fix" id="modal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content h-full">
-                    <button class="close" data-dismiss="modal" type="button" aria-label="Close" />
+                    <button class="close bg-danger" data-dismiss="modal" type="button" aria-label="Close" />
 
-                    <div class="container">
-                        <div class="row">
+                    <div class="container h-full">
+                        <div class="row align-middle">
                             <div class="col-md-8 offset-md-2">
                                 <ProjectView project={ project } />
                             </div>
@@ -31,31 +31,22 @@ pub struct ProjectViewProps {
 pub fn project(ProjectViewProps { project }: &ProjectViewProps) -> Html {
     if let Some(project) = project {
         html! {
-            <>
-                <section id="project">
+            <div class="text-center">
+                <div class="mb-20">
+                    <p class="text-center font-size-24 m-0 mb-5">{ project.name.clone() }</p>
                     <div>
-                        <img src="" alt="Project Image" class="m-5 w-500 p-0" />
-                        <p class="text-center font-size-16 m-0 mb-5">{ project.name.clone() }</p>
-                        <div></div>
-                        <div class="d-flex justify-content-center">
-                            <a href="https://github.com/Dragnansia"><i style="color: #ddd" class="font-size-20 fa fa-github"></i></a>
-                        </div>
+                        { images(project.images.clone()) }
                     </div>
-                </section>
+                    <div class="d-flex justify-content-center">
+                        <a href="https://github.com/Dragnansia"><i style="color: #ddd" class="font-size-20 fa fa-github"></i></a>
+                    </div>
+                </div>
 
-                <section id="information">
-                    { project.description.clone() }
-                </section>
-
-                <section id="photos">
-                    { images(project.images.clone()) }
-                </section>
-            </>
+                { project.description.clone() }
+            </div>
         }
     } else {
-        html! {
-            <div class="lds-dual-ring" />
-        }
+        html! {}
     }
 }
 
